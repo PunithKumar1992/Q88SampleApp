e1704bc188311b4a9ce84d3e4ad4b203c966f12c
Punithkumar <Punithkumar@BSOLDHN-013.bsol.com>
2019-10-04 14:04:55 +0530

    First Commit by Punith

---
+++ src/main/java/com/q88/sample/Q88SampleAppApplication.java
@@ -0,0 +1,15 @@
+package com.q88.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Q88SampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q88SampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/q88/sample/Q88SampleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.q88.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Q88SampleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
